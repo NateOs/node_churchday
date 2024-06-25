6ee37581caa4de2bd3170f821eee25366d36585c
nateos <sodjanathan@gmail.com>
2024-06-25 17:09:52 +0000

    fix: removed /url from origin url

--- utils/sendPasswordResetEmail.js
+++ utils/sendPasswordResetEmail.js
@@ -3,7 +3,7 @@ const sendEmail = require("./sendEmail");
 const sendPasswordResetEmail = async ({
   name, token, email, origin,
 }) => {
-  const resetURL = `${origin}/user/reset-password?email=${email}&token=${token}`;
+  const resetURL = `${origin}/reset-password?email=${email}&token=${token}`;
   const message = ` <a href="${resetURL}">Reset link here</a> </p>`;
   await sendEmail({
     to: email,
